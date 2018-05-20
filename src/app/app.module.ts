395051258359968831dc3f1cfaed7ffe8655c2b1
Max Kroshka <mkroshka@Maxs-iMac.local>
2018-05-19 17:15:07 -0700

    Add toolbar with future search and filter capabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule( {
   declarations: [
     AppComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [ AppComponent ]
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+interface Filter {
+  name : string;
+}
+
+@Component( {
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: [ './toolbar.component.scss' ],
+} )
+export class ToolbarComponent implements OnInit {
+  public searchForm : FormControl = new FormControl();
+  public selectedFilter : Filter;
+  public filters : Filter[] = [
+    {
+      name : 'Popularity',
+    },
+    {
+      name : 'Name',
+    },
+    {
+      name : 'Year',
+    },
+  ];
+
+  ngOnInit() : void {
+    this.selectedFilter = this.filters[ 0 ].name;
+  }
+
+}
